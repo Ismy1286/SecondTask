1a9f07574365e75e830d01ce8115f7d2e88a22a3
Ainur <ismy1286@gmail.com>
2021-12-23 15:08:23 +0600

    first commit

--- SecondTask/ViewController.swift
+++ SecondTask/ViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
+import SnapKit
+
 
 class ViewController: UIViewController {
 
+    private var textField = UITextField()
+    private var button = UIButton()
+    private var label = UILabel()
+    
+    var s = ""
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        textField.placeholder = "Введите текст..."
+        
+        view.addSubview(textField)
+        textField.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(-120)
+            make.width.equalToSuperview().offset(-100)
+        }
+        
+        button.backgroundColor = .yellow
+        button.setTitle("Print", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        button.addTarget(nil, action: #selector(clickButton(sender:)), for: .touchUpInside)
+        
+        view.addSubview(button)
+        button.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.centerY.equalToSuperview().offset(-70)
+            make.width.equalTo(100)
+        }
+        
+        label.numberOfLines = 0
+        label.lineBreakMode = .byCharWrapping
+        
+        view.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(button.snp.bottom).offset(20)
+            make.width.equalToSuperview().offset(-100)
+        }
     }
-
-
+    @objc func clickButton(sender: UIButton) {
+        
+        s = String(textField.text ?? "")
+        let b = s.replacingOccurrences(of: ", ", with: "\n", options: .literal, range: nil)
+       
+        label.text = b
+    }
 }
-
